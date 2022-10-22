8182a817d927268cc8a3998dfe21b6a2ff94bfd2
Shryesth Pandey <shryesth4236@gmail.com>
2022-10-23 00:38:46 +0530

    Updated for TiX

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,17 @@
 import Link from 'next/link';
-
+import Image from '../LOGO (1).png';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <div>
+        <img
+          style={{ width: '15rem', height: '10rem', objectFit: 'contain' }}
+          src="https://drive.google.com/uc?id=1PF-VOm6FegyY8oDuzfhV_107dmJgq_QT"
+          alt=""
+        />
+      </div>
+
+      {/* <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" /> */}
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,10 @@ export default function Index({ posts, globalData }) {
       <SEO title={globalData.name} description={globalData.blogTitle} />
       <Header name={globalData.name} />
       <main className="w-full">
-        <h1 className="text-3xl lg:text-5xl text-center mb-12">
+        <h1
+          style={{ fontSize: '1.4rem' }}
+          className="text-3xl lg:text-5xl text-center mb-12"
+        >
           {globalData.blogTitle}
         </h1>
         <ul className="w-full">
